385eb1a6b94da24643ee1e664fbd385d3bef1461
Sagar10041 <sagar10041@gmail.com>
2025-06-24 21:31:50 +0545

    added models

---
+++ Models/Birthdays.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Birthday_Remainder_Backend.Models
+{
+    public class Birthdays
+    {
+        public int Id;
+
+        public string Names;
+
+        public int age;
+
+        public 
+
+    }
+}
